2b7cc445966363b630659f0c51b3c38b17e5fc7f
kostili-tec <weh4teyou@gmail.com>
2022-01-27 23:18:14 +0300

    feat: made script for portfolio buttons & imges

--- portfolio/index.js
+++ portfolio/index.js
@@ -13,3 +13,38 @@
         menu.classList.remove('nav__active');
     })
 }());
+
+const btnWinter = document.querySelector('.winter-button');
+const portfolioBtns = document.querySelector('.portfolio-buttons'); // div buttons-container
+const portfolioBtn = document.querySelectorAll('.portfolio-button');
+const portfolioImages = document.querySelectorAll('.portfolio-img');
+
+function changeImage(event) {
+    if (event.target.classList.contains('portfolio-button')) {
+        console.log(event.target)
+    }
+}
+
+function changeClassActive(element){
+    element.forEach(item => {
+        item.classList.remove('active');
+    });
+    this.event.target.classList.add('active');
+    
+    
+}
+
+// btnWinter.addEventListener('click', () => {
+//     portfolioImages.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`)
+// })
+
+portfolioBtns.addEventListener('click', event => {
+    if(event.target.classList.contains('portfolio-button')) {
+        let nameButton = event.target.dataset.season;
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${nameButton}/${index + 1}.jpg`)
+        console.log(nameButton);
+        changeClassActive(portfolioBtn);        
+    }    
+})
+
+
