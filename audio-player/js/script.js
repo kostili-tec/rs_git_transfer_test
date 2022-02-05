199ccefbfb89f060d0deb9dd78ef7a0568171efe
kostili-tec <weh4teyou@gmail.com>
2022-02-05 19:30:39 +0300

    feat: working - play-pause, back, next buttons, add new tracks

--- audio-player/js/script.js
+++ audio-player/js/script.js
@@ -2,16 +2,15 @@
 const coverImg = document.querySelector('.album-cover__img');
 const songName = document.querySelector('.song-name__h1');
 const playBtn = document.querySelector('.play-btn');
-const pauseBtnTest = document.querySelector('.pause-btn'); // PAUSE
 const pauseBtn = document.querySelector('.stop-btn');
 const prevBtn = document.querySelector('.backward-btn');
 const nextBtn = document.querySelector('.forward-btn');
+const volumeBtn = document.querySelector('.volume-btn');
 const playSvg = document.querySelector('.icon-play');
 const progressBar = document.querySelector('.progress-bar');
 const progress = document.querySelector('.progress');
 const audioCurrentTime = document.querySelector('.current-time');
 const audioFullLength = document.querySelector('.full-length');
-// const audio = new Audio('../audio-player/assets/audio/loaded.mp3');
 const audio = new Audio();
 let isPlay = false;
 
@@ -33,43 +32,62 @@ const songs = [
         audioSrs: `../audio-player/assets/audio/dark_all_day.mp3`    
     }, {
         index: 3,
-        name: ``,
-        coverImg: `../audio-player/assets/covers/`,
-        audioSrs: `../audio-player/assets/audio/`    
+        name: `Carpenter Brut – Night Stalker`,
+        coverImg: `../audio-player/assets/covers/night_stalker.jpg`,
+        audioSrs: `../audio-player/assets/audio/night_stalker.mp3`    
     }, {
         index: 4,
-        name: ``,
-        coverImg: `../audio-player/assets/covers/`,
-        audioSrs: `../audio-player/assets/audio/`
+        name: `the Midnight - Nocturnal`,
+        coverImg: `../audio-player/assets/covers/nocturnal.jpg`,
+        audioSrs: `../audio-player/assets/audio/nocturnal.mp3`
     }
 ]
 
-let current = 1;
+let current = 0;
 audio.src = songs[current].audioSrs;
-console.log(songs[current].name);
 
 // =============>>> EVENTS <<<============= //
-playBtn.addEventListener('click', playPauseSong);
-pauseBtnTest.addEventListener('click', pauseAudio);
+playBtn.addEventListener('click', playPauseAudio);
 audio.addEventListener('timeupdate', fillProgress);
 progressBar.addEventListener('click', updateProgress);
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
-// pauseBtn.addEventListener('click', pauseAudio);
+audio.addEventListener('ended', nextSong);
+
 
 // console.log(playSvg.innerHTML);
 
 // =============>>> FUNCTIONS <<<============= //
 
-function playPauseSong(){
-    console.log(songs[current].name);
+function playPauseSong(){   
     if (audio.duration > 0 && !audio.paused) {
         audio.pause();
         playSvg.innerHTML = `<use xlink:href="./assets/svg/sprite.svg#media-pause"></use>`;        
-    } else {
-        //audio.src = '../audio-player/assets/audio/loaded.mp3';
+    } else {        
         audio.play();
         playSvg.innerHTML = `<use xlink:href="./assets/svg/sprite.svg#media-play"></use>`;
+    }
+}
+
+function playAudio() {
+    audio.src = songs[current].audioSrs;
+    audio.currentTime = 0;
+    audio.play();
+    isPlay = true;   
+}
+function pauseAudio() {
+    audio.pause();
+    isPlay = false;   
+}
+function playPauseAudio(){
+    if (isPlay) {       
+        audio.pause();
+        isPlay = false;
+        playSvg.innerHTML = `<use xlink:href="./assets/svg/sprite.svg#media-play"></use>`; 
+    } else if (!isPlay) {  
+        audio.play();
+        isPlay = true;
+        playSvg.innerHTML = `<use xlink:href="./assets/svg/sprite.svg#media-pause"></use>`;
     }
 }
 
@@ -86,13 +104,13 @@ function playNextOrBack(){
         current = 0;
     } else if (current < 0) {
         current = songs.length - 1;
-    }
+    }   
     songName.textContent = songs[current].name;
     audio.srs = songs[current].audioSrs;
-    coverImg.src = songs[current].coverImg;
-    audio.play();
+    coverImg.src = songs[current].coverImg;    
+    playAudio();   
     
-    console.log(audio.srs);
+    // console.log(audio.srs);
 }
 
 function fillProgress(e) {
@@ -122,14 +140,3 @@ function fillProgress(e) {
 function updateProgress(e) {
     audio.currentTime = (e.offsetX / this.clientWidth) * audio.duration;
 }
-
-function playAudio() {
-    audio.src = '/porfolio/kostili-tec-JSFEPRESCHOOL/audio-player/assets/audio/loaded.mp3'
-    audio.currentTime = 0;
-    audio.play();
-}
-function pauseAudio() {
-    audio.pause();
-    console.log('click');
-}
-
