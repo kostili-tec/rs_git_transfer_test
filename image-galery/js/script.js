1368376772970f7e536d2f3427b36e086f0d4276
kostili-tec <weh4teyou@gmail.com>
2022-02-12 19:39:13 +0300

    feat: add next/prev page buttons, fix queries obj, did find button

--- image-galery/js/script.js
+++ image-galery/js/script.js
@@ -2,45 +2,79 @@ const findBtn = document.querySelector('.find');
 const gridDiv = document.querySelector('.grid');
 const input = document.getElementById('input');
 const testBtn = document.querySelector('.test-btn');
+const nav = document.querySelector('.nav');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
 const url = 'https://api.unsplash.com/photos/?client_id=FYdN3NPqRZfJlZqP-pOElRcZ95ARwjpFvreQ-1zSBOM';
-
+let numberPage = 1; 
+let = inputValue = '';
+let = btnShows = false;
 const quaries = {
     url: 'https://api.unsplash.com/',
     photos: 'photos/',
+    searchPhotos: 'search/photos',
     oneRandom: 'photos/random?query=spring',
     tenRandom: 'photos/random?query=spring&count=20',
-    perPage: '&per_page=10',
-    searchPhotos: 'search/photos',
-    searchQuarie: '?query=',
-    inputValue: '',
-    testVal: 'https://api.unsplash.com/search/photos?query=spring',
+    page: `&page=${numberPage}`,
+    perPage: '&per_page=12',
+    searchQuary: `?query=${inputValue}`,    
+    testVal: 'https://api.unsplash.com/search/photos?query=winter',
     clientId: '&client_id=FYdN3NPqRZfJlZqP-pOElRcZ95ARwjpFvreQ-1zSBOM',
 }
 const testUrl = quaries.url + quaries.tenRandom + quaries.clientId;
 const testOne = quaries.url + quaries.oneRandom + quaries.clientId;
 let findUrl;
-console.log(testOne)
+// console.log(testOne)
 // console.log(quaries.url + quaries.clientId);
-console.log(input.value);
+// console.log(input.value);
 
-findBtn.addEventListener('click', getData);
+document.addEventListener('DOMContentLoaded', showHideBtns);
+
+// findBtn.addEventListener('click', getData);
+findBtn.addEventListener('click', () => {
+    checkInput();
+    getData(findUrl);
+});
 testBtn.addEventListener('click', () => {
-    quaries.inputValue = input.value; 
-    console.log(quaries.inputValue);
-    console.log(quaries);
+    inputValue = input.value; 
+    quaries.searchQuary = `?query=${inputValue}`
+    console.log('inputValue = ' + inputValue);
+    console.log('searchQuary = ' + quaries.searchQuary);
+    // console.log(quaries);
     // findUrl = quaries.url + quaries.searchPhotos + quaries.searchQuarie + quaries.inputValue + quaries.perPage + quaries.clientId;
-    findUrl = quaries.testVal + quaries.clientId;
+
+    // findUrl = quaries.testVal + quaries.perPage + quaries.page + quaries.clientId; //worked
+    // https://api.unsplash.com/search/photos?query=winter&per_page=11&page=1&client_id=FYdN3NPqRZfJlZqP-pOElRcZ95ARwjpFvreQ-1zSBOM
+
+    findUrl = quaries.url + quaries.searchPhotos + quaries.searchQuary + quaries.page + quaries.perPage + quaries.clientId;
     console.log(findUrl);
+    // console.log(quaries.page);
+});
+prevBtn.addEventListener('click', () => {
+    numberPage--;
+    if (numberPage < 0) {
+        numberPage = 0;
+    }
+    quaries.page = `&page=${numberPage}`;
+    console.log(quaries.page);
+});
+
+nextBtn.addEventListener('click', () => {
+    numberPage++;
+    quaries.page = `&page=${numberPage}`;
+    console.log(numberPage);
+    console.log(quaries.page);
 });
 // testBtn.addEventListener('click', testFunc);
 
-async function getData() {
-    const res = await fetch(findUrl);
+async function getData(url) {
+    const res = await fetch(url);
     const data = await res.json();
     console.log(typeof(data));
     console.log(data);
-    showData(data);
+    // showData(data);
+    showDataFor(data);
     // showOne(data);
 }
 
@@ -70,10 +104,45 @@ function showData(link){
     console.log(Object.keys(link).length);
 }
 
+function showDataFor(link) {
+    clearPage();
+    link.results.forEach(item => {
+
+        const imgDiv = document.createElement('div');
+        imgDiv.classList.add('img');
+        imgDiv.style.backgroundImage = `url(${item.urls.regular})`;
+        gridDiv.append(imgDiv);
+        // console.log(item);
+    })
+    btnShows = true;
+    showHideBtns();
+}
+function clearPage(){
+    let imgDiv = document.querySelectorAll('.img');
+    console.log(imgDiv);
+        imgDiv.forEach(item => {
+            item.remove();
+        });
+    btnShows = false;
+    showHideBtns();
+}
+function checkInput(){
+    inputValue = input.value; 
+    quaries.searchQuary = `?query=${inputValue}`;
+    findUrl = quaries.url + quaries.searchPhotos + quaries.searchQuary + quaries.page + quaries.perPage + quaries.clientId;
+    console.log(findUrl);
+}
+function showHideBtns(){
+    if (btnShows == false) {
+        nav.classList.add('hide');
+    } else {
+        nav.classList.remove('hide');
+    }
+}
 function testFunc() {
     quaries.inputValue = input.value; 
     console.log(quaries.inputValue);
     console.log(quaries);
-    findUrl = quaries.url + quaries.photos + quaries.searchQuarie + quaries.inputValue + quaries.clientId;
+    findUrl = quaries.url + quaries.photos + quaries.searchQuary + quaries.inputValue + quaries.clientId;
     console.log(findUrl);
 }
