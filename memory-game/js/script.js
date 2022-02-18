93a70f3f413ce0fb31bc8c223bdc06b32615271f
kotili-tec <weh4teyou@gmail.com>
2022-02-18 16:56:02 +0300

    feat: cards, style animations, flip function

---
+++ memory-game/js/script.js
@@ -0,0 +1,21 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    hasFlippedCard = false;
+}
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+console.log('test');
